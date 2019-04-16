7c764acefc110cb5ba81809b500e63b111b86bf2
Will Hutwagner <36306338+whutwagner3@users.noreply.github.com>
2019-04-16 19:10:00 -0400

    Update APIcall_2.py

--- APIcall_2.py
+++ APIcall_2.py
@@ -2,6 +2,7 @@ from Bio import Entrez
 import ensembl_rest
 import sys
 
+final={}
 
 def fetch_details(id_list):
     ids = ','.join(id_list)
@@ -22,12 +23,12 @@ handle = Entrez.esearch(db="pubmed",retmax=13,id=ied,term=organism)
 record = Entrez.read(handle)
 handle.close()
 print(record['IdList'])
+final['PubMedIDs']=record['IdList']
 id_list = record['IdList']
 papers = fetch_details(id_list)
 print(papers)
 '''for i, paper in enumerate(papers):
 	print()'''
-final={}
 final['ids']=[]
 final['seq']=[]
 final['papers_id']=record['IdList']
