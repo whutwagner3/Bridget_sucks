505b4346a9757a541ad42e478fc709b3997370f6
bneary3 <49699691+bneary3@users.noreply.github.com>
2019-04-16 19:18:11 -0400

    Update APIcall_2.py
    put tabs in...

--- APIcall_2.py
+++ APIcall_2.py
@@ -5,13 +5,13 @@ import sys
 final={}
 
 def fetch_details(id_list):
-    ids = ','.join(id_list)
-    Entrez.email = sys.argv[0]
-    handle = Entrez.efetch(db='pubmed',
-                           retmode='xml',
-                           id=ids)
-    results = Entrez.read(handle)
-    return results
+	ids = ','.join(id_list)
+	Entrez.email = sys.argv[0]
+	handle = Entrez.efetch(db='pubmed',
+			       retmode='xml',
+			       id=ids)
+	results = Entrez.read(handle)
+	return results
 def module_2_api_call()
 	organism='aedes aegypti'
 	geneIDs=['AAEL003439', 'AAEL011411', 'AAEL012522', 'AAEL012774', 'AAEL012864', 'AAEL013263']
