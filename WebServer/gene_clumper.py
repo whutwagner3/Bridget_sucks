96286e87c8a6d27693bbbe85d8f7f88c9ac24dac
bneary3 <49699691+bneary3@users.noreply.github.com>
2019-04-25 11:18:47 -0400

    Fixed/deduplicated output

--- WebServer/gene_clumper.py
+++ WebServer/gene_clumper.py
@@ -8,9 +8,9 @@ import biomart
 import json
 import sys
 import time
-import re
+import pickle
 
-def main(gene_ids='', organism='Aedes aegypti (LVP_AGWG) genes (AaegL5.2)'):
+def main(gene_ids='', organism='Aedes aegypti (LVP_AGWG) genes (AaegL5.2)', save_vars=False):
     if gene_ids == '':
         # 195 genes (74 with GO terms)
         gene_ids = ['AAEL000243', 'AAEL000853', 'AAEL000964', 'AAEL001015', 'AAEL001076', 'AAEL001118', 'AAEL001199', 'AAEL001208', 'AAEL001682', 'AAEL001714', 'AAEL001821', 'AAEL002024', 'AAEL002303', 'AAEL002380', 'AAEL002641', 'AAEL003265', 'AAEL003436', 'AAEL003755', 'AAEL003982', 'AAEL004037', 'AAEL004648', 'AAEL004717', 'AAEL004991', 'AAEL005133', 'AAEL005164', 'AAEL005287', 'AAEL005399', 'AAEL005420', 'AAEL005759', 'AAEL006137', 'AAEL006335', 'AAEL006365', 'AAEL006690', 'AAEL007005', 'AAEL007110', 'AAEL007437', 'AAEL007598', 'AAEL007604', 'AAEL007719', 'AAEL007951', 'AAEL008266', 'AAEL008282', 'AAEL008360', 'AAEL008409', 'AAEL009517', 'AAEL009724', 'AAEL010140', 'AAEL010424', 'AAEL010749', 'AAEL010915', 'AAEL010987', 'AAEL011112', 'AAEL011160', 'AAEL011757', 'AAEL011777', 'AAEL011808', 'AAEL011926', 'AAEL011966', 'AAEL011987', 'AAEL012139', 'AAEL012356', 'AAEL012703', 'AAEL012713', 'AAEL012887', 'AAEL013024', 'AAEL013054', 'AAEL013216', 'AAEL013232', 'AAEL013856', 'AAEL014334', 'AAEL014579', 'AAEL015201', 'AAEL015457', 'AAEL015526', 'AAEL017095', 'AAEL017557', 'AAEL018061', 'AAEL019417', 'AAEL019494', 'AAEL019498', 'AAEL019587', 'AAEL019675', 'AAEL019676', 'AAEL019960', 'AAEL020084', 'AAEL020179', 'AAEL020315', 'AAEL020404', 'AAEL020424', 'AAEL020504', 'AAEL020630', 'AAEL020683', 'AAEL020691', 'AAEL021030', 'AAEL021152', 'AAEL021364', 'AAEL021379', 'AAEL021408', 'AAEL021504', 'AAEL021521', 'AAEL021559', 'AAEL021712', 'AAEL021769', 'AAEL021772', 'AAEL021808', 'AAEL021934', 'AAEL022018', 'AAEL022176', 'AAEL022448', 'AAEL022473', 'AAEL022536', 'AAEL022592', 'AAEL022672', 'AAEL022676', 'AAEL022688', 'AAEL022767', 'AAEL022871', 'AAEL022975', 'AAEL023049', 'AAEL023054', 'AAEL023067', 'AAEL023079', 'AAEL023139', 'AAEL023140', 'AAEL023170', 'AAEL023447', 'AAEL023573', 'AAEL023607', 'AAEL023725', 'AAEL023771', 'AAEL023775', 'AAEL023878', 'AAEL023890', 'AAEL024058', 'AAEL024096', 'AAEL024122', 'AAEL024457', 'AAEL024648', 'AAEL024680', 'AAEL024689', 'AAEL024903', 'AAEL024944', 'AAEL025159', 'AAEL025251', 'AAEL025255', 'AAEL025387', 'AAEL025442', 'AAEL025454', 'AAEL025488', 'AAEL025633', 'AAEL025707', 'AAEL025713', 'AAEL025860', 'AAEL025928', 'AAEL025932', 'AAEL025971', 'AAEL026268', 'AAEL026400', 'AAEL026451', 'AAEL026530', 'AAEL026547', 'AAEL026586', 'AAEL026611', 'AAEL026692', 'AAEL026747', 'AAEL026756', 'AAEL026766', 'AAEL026850', 'AAEL027013', 'AAEL027061', 'AAEL027139', 'AAEL027200', 'AAEL027220', 'AAEL027224', 'AAEL027235', 'AAEL027273', 'AAEL027312', 'AAEL027356', 'AAEL027505', 'AAEL027692', 'AAEL027814', 'AAEL027820', 'AAEL027849', 'AAEL027889', 'AAEL027904', 'AAEL027925', 'AAEL028054', 'AAEL028067', 'AAEL028074', 'AAEL028093', 'AAEL028094', 'AAEL028122', 'AAEL028210', 'AAEL028219', 'AAEL028242']
@@ -23,9 +23,24 @@ def main(gene_ids='', organism='Aedes aegypti (LVP_AGWG) genes (AaegL5.2)'):
     else:
         try: gene_ids = gene_ids.split(',')
         except AttributeError: pass
+    if save_vars:
+        with open('./variables/gene_ids.pkl') as f:
+            pickle.dump(gene_ids, f)
+
     dataset = species2dataset()[organism]
     gene2go_list = get_go(gene_ids, dataset)
+    if save_vars: 
+        with open('./variables/gene2go.pkl') as f:
+            pickle.dump(gene2go_list, f)
+
     distances, base2gene, genealogy = build_ontologies(gene2go_list)
+    if save_vars: 
+        with open('./variables/distances.pkl') as f:
+            pickle.dump(distances, f)
+        with open('./variables/base2gene.pkl') as f:
+            pickle.dump(base2gene, f)
+        with open('./variables/genealogy.pkl') as f:
+            pickle.dump(genealogy, f)
     
     real_genealogy = {}
     for ont, thing in genealogy.items():
@@ -36,19 +51,35 @@ def main(gene_ids='', organism='Aedes aegypti (LVP_AGWG) genes (AaegL5.2)'):
     go_edges = get_edges(distances)
 
     real_edges = []
+    pylinks = []
     for edge in go_edges:
         sources = base2gene[edge['source']]
         targets = base2gene[edge['target']]
         value = edge['value']
         for src in sources:
             for tgt in targets:
+                pylinks.append((src, tgt, value))
                 real_edges.append({'source': src,
                                    'target': tgt,
                                    'value': value})
 
-    with open('./WebServer/static/graph_json_vars.json', 'w') as f:
-        f.write('const genealogy = ' + json.dumps(real_genealogy, indent=2) + '\n')
-        f.write('const graph = ' + json.dumps(real_edges, indent=2) + '\n')
+    if save_vars:
+        with open('./variables/real_genealogy.pkl') as f:
+            pickle.dump(real_genealogy, f)
+        with open('./variables/go_edges.pkl') as f:
+            pickle.dump(go_edges, f)
+        with open('./variables/real_edges.pkl') as f:
+            pickle.dump(real_edges, f)
+        with open('./variables/pylinks.pkl') as f:
+            pickle.dump(pylinks, f)
+
+    with open('./static/genealogy.json', 'w') as f:
+        json.dump(real_genealogy, f)
+
+    with open('./static/links.json', 'w') as f:
+        json.dump(real_edges, f)
+
+    return pylinks
 
 def print_time(sec, message='', to_print=True):
     if message != '': message += ': '
@@ -58,15 +89,26 @@ def print_time(sec, message='', to_print=True):
     return output
 
 def get_edges(distances):
+    '''
+    Gets a list of links for the graph from input dict
+    Input: dict of base onts: list of (dist from base, parent ID)s
+    Output: JSON object list with 'source', 'target', and 'value'
+    '''
     links = []
     for ont1 in distances:
         for ont2 in distances:
+            if ont1 == ont2: continue
+            match_found = False
             for dist1, term1 in sorted(distances[ont1]):
                 for dist2, term2 in sorted(distances[ont2]):
                     if term1 != term2: continue
+                    match_found = True
                     links.append({'source': ont1,
                                   'target': ont2,
                                   'value': dist1 + dist2})
+                    break
+                if match_found: break
+            if match_found: break
     return links
 
 def add_entry(key, value, dic):
@@ -87,8 +129,9 @@ def build_ontologies(gene2go):
     '''
     Queries the GO database for all parent GO IDs
     Input: dictionary of gene ID: list of relevant GO IDs
-    Returns: [edge_list (list of tuples: (child GO ID, parent GO ID)),
-              goid2goterm (dict of GO IDs to GO terms)]
+    Returns: [dist (dict of childGO: list of (parent to child distance, parentID)),
+              basegos2gene (dict of GO IDs to genes with this base GO),
+              genealogy (JSON-like object for output to file)]
     '''
 
     # make paths (multiples included)
@@ -193,4 +236,7 @@ if __name__ == '__main__':
         main()
     else:
         main(sys.argv[1], sys.argv[2])
-    print_time(time.time() - lap)
+#    print_time(time.time() - lap, 'Completed in')
+    print_time(time.time() - lap, to_print=False)
+
+
